dc0dad9b48321db50926d6f1c84d612ed77e3e0c
akash20182019 <45789225+akash20182019@users.noreply.github.com>
2018-12-18 15:13:17 +0530

    till pf

---
+++ Libs/CustomKeywords.groovy
@@ -0,0 +1,23 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import com.kms.katalon.core.testobject.TestObject
+
+import java.lang.String
+
+
+def static "click1.click2.run"(
+    	TestObject objectto	) {
+    (new click1.click2()).run(
+        	objectto)
+}
+
+def static "click1.upload.uploadFile"(
+    	TestObject to	
+     , 	String filePath	) {
+    (new click1.upload()).uploadFile(
+        	to
+         , 	filePath)
+}
